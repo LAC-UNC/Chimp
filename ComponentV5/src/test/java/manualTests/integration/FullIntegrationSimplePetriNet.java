0240deeb7cf839a4ce45d433fbb65146685ea93c
aaquaran <aaquaran@AAQUARAN-MOBL2.amr.corp.intel.com>
2014-10-15 21:40:04 -0300

    Adding simple full integration manual test

--- ComponentV5/src/test/java/manualTests/integration/FullIntegrationSimplePetriNet.java
+++ ComponentV5/src/test/java/manualTests/integration/FullIntegrationSimplePetriNet.java
@@ -2,6 +2,7 @@ package manualTests.integration;
 
 import java.net.URISyntaxException;
 
+import com.lac.petrinet.components.Dummy;
 import com.lac.petrinet.configuration.providers.ConfigurationFileTest;
 import com.lac.petrinet.configuration.providers.PNMLConfigurationReader;
 import com.lac.petrinet.core.PetriNet;
@@ -9,10 +10,50 @@ import com.lac.petrinet.exceptions.PetriNetException;
 
 public class FullIntegrationSimplePetriNet {
 
+	public static int sharedCounter = 0; //Shared resource
+	public static String last = ""; //Shared resource
+	
+	public static class Counter extends Dummy {
+		private String msg;
+		
+		public Counter(PetriNet pn, String tName, String message){
+			super(pn, tName);
+			this.msg = message;
+		}
+		
+		@Override
+		protected void execute() {
+			FullIntegrationSimplePetriNet.sharedCounter++;
+			FullIntegrationSimplePetriNet.last = this.msg;
+			System.out.println(this.msg);
+			
+			return;
+		}
+	}
+	
 	public static void main(String[] args) {
 		PNMLConfigurationReader pnmlConfigurator = new PNMLConfigurationReader();
+		
+
 		try {
 			PetriNet pn = pnmlConfigurator.loadConfiguration(getJarpath() + "pnml/simplePnml.pnml");
+			System.out.println(pn.getInformed("t0").toString());
+			
+			FullIntegrationSimplePetriNet.Counter dumb1 = new FullIntegrationSimplePetriNet.Counter(pn,"t1", "Dumb1");
+			FullIntegrationSimplePetriNet.Counter dumb2 = new FullIntegrationSimplePetriNet.Counter(pn,"t2", "Dumb2");
+			
+			pn.assignDummy("t0", dumb1);
+			pn.assignDummy("t1", dumb2);
+			
+			pn.nextCicle();
+			pn.nextCicle();
+			pn.nextCicle();
+			pn.nextCicle();
+			pn.nextCicle();
+			pn.nextCicle();
+			pn.nextCicle();
+			pn.nextCicle();
+			pn.nextCicle();
 		} catch (PetriNetException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
